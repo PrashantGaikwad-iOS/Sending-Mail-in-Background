25c82a8f16fd7ca57c9f9554bac84dbf7e1c89f3
Prashant Gaikwad <itsmeprashantg@gmail.com>
2019-11-02 00:06:23 +0530

    Add files via upload

---
+++ sendEmail_bridge.h
@@ -0,0 +1,13 @@
+//
+//  sendEmail_bridge.h
+//  Sending Mail in Background
+//
+//  Created by Prashant G on 1/23/19.
+//  Copyright © 2019 Prashant G. All rights reserved.
+//
+
+#ifndef sendEmail_bridge_h
+#define sendEmail_bridge_h
+#import <MailCore/MailCore.h>
+
+#endif /* sendEmail_bridge_h */
